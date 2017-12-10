beab7c9834e99019f399dd56dbdf349749792b05
huangl07 <290012645@qq.com>
2017-12-10 16:07:57 +0800

    biostacks

---
+++ bin/step06.correct.pl
@@ -0,0 +1,115 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+my $BEGIN_TIME=time();
+use Getopt::Long;
+my ($ulist,$clist,$dOut,$dShell,$slist);
+use Data::Dumper;
+use FindBin qw($Bin $Script);
+use File::Basename qw(basename dirname);
+my $version="1.0.0";
+GetOptions(
+	"help|?" =>\&USAGE,
+	"ulist:s"=>\$ulist,
+	"clist:s"=>\$clist,
+	"slist:s"=>\$slist,
+	"out:s"=>\$dOut,
+	"dsh:s"=>\$dShell,
+			) or &USAGE;
+&USAGE unless ($ulist and $clist and $slist and $dOut and $dShell);
+mkdir $dOut if (!-d $dOut);
+mkdir $dShell if(!-d $dShell);
+$dOut=ABSOLUTE_DIR($dOut);
+$dShell=ABSOLUTE_DIR($dShell);
+mkdir "$dOut/stacks" if (!-d "$dOut/stacks");
+
+open In,$ulist;
+open Out,">$dOut/ustacks.list";
+open SH,">$dShell/step06.correct2.sh";
+while (<In>) {
+	chomp;
+	next if ($_ eq ""||/^$/);
+	my ($sample,$ustacks)=split(/\s+/,$_);
+	`ln -s $ustacks* $dOut/stacks`;
+	my $ustack=basename($ustacks);
+	print Out "$sample $dOut/$ustack\n";
+	print SH "perl $Bin/bin/tag-stat.pl -i $dOut/$sample -o $dOut/$sample.tags.stat -k $sample\n";
+}
+close In;
+close SH;
+close Out;
+open In,$clist;
+while (<In>) {
+	chomp;
+	next if ($_ eq ""||/^$/);
+	my ($sample,$cstacks)=split(/\s+/,$_);
+	`ln -s $cstacks* $dOut/stacks`;
+}
+close In;
+open In,$slist;
+while (<In>) {
+	chomp;
+	next if ($_ eq ""||/^$/);
+	my ($sample,$sstacks)=split(/\s+/,$_);
+	`ln -s $sstacks* $dOut/stacks`;
+}
+close In;
+open SH,">$dShell/step06.correct1.sh";
+print SH "rxstacks -P $dOut/stacks/ --conf_lim 0.25 --prune_haplo --model_type bounded --bound_high 0.1 --lnl_lim -10.0 -t 16 -o $dOut/ && ";
+close SH;
+
+my $job="perl /mnt/ilustre/users/dna/.env//bin//qsub-sge.pl --Queue dna --Resource mem=80G --CPU 16 --Nodes 1 $dShell/step06.correct1.sh";
+print  "$job\n";
+`$job`;
+print "$job\tdone!\n";
+my $job="perl /mnt/ilustre/users/dna/.env//bin//qsub-sge.pl --Queue dna --Resource mem=80G --CPU 16 --Nodes 1 $dShell/step06.correct2.sh";
+print  "$job\n";
+`$job`;
+print "$job\tdone!\n";
+
+#######################################################################################
+print STDOUT "\nDone. Total elapsed time : ",time()-$BEGIN_TIME,"s\n";
+#######################################################################################
+sub ABSOLUTE_DIR #$pavfile=&ABSOLUTE_DIR($pavfile);
+{
+	my $cur_dir=`pwd`;chomp($cur_dir);
+	my ($in)=@_;
+	my $return="";
+	if(-f $in){
+		my $dir=dirname($in);
+		my $file=basename($in);
+		chdir $dir;$dir=`pwd`;chomp $dir;
+		$return="$dir/$file";
+	}elsif(-d $in){
+		chdir $in;$return=`pwd`;chomp $return;
+	}else{
+		warn "Warning just for file and dir \n$in";
+		exit;
+	}
+	chdir $cur_dir;
+	return $return;
+}
+
+sub USAGE {#
+        my $usage=<<"USAGE";
+Contact:        long.huang\@majorbio.com;
+Script:			$Script
+Description:
+	fq thanslate to fa format
+	eg:
+	perl $Script -i -o -k -c
+
+Usage:
+  Options:
+	-ulist	<file>	ustacks list
+	-clist	<file>	cstacks	list
+	-slist	<file>	sstacks list
+	-group	<file>	group list for cstacks
+	-out	<dir>	output dir
+	-dsh	<dir>	work shell dir
+	-h         Help
+
+USAGE
+        print $usage;
+        exit;
+}
